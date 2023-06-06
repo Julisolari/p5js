d33608b105c5609deccfa2775023ff1cd33ca94b
Juli <127170019+Julisolari@users.noreply.github.com>
2023-06-06 16:01:01 -0300

    Helena Sarin

---
+++ sketch.js
@@ -0,0 +1,15 @@
+let img; //variable ´img´
+
+function setup() {
+  createCanvas(512, 512);
+  img = loadImage('artesmultimediales/pezD.png'); //imagen de un pez chino
+}
+
+function draw() {
+  background("#fff");
+}
+
+  image(img, 0, 0);
+  // Muestra la imagen en la posición (0, height/2) a la mitad del tamaño
+  image(img, 0, height / 2, img.width / 2, img.height / 2);
+}
